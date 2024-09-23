399f144d9091a35207ad8736e57a02d452e72961
Griffin <omondigriffin4@gmail.com>
2024-09-23 15:39:50 +0200

    creates login and registration views

--- users/views.py
+++ users/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from .serializers import UserRegistrationSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
 
-def users(request):
-    return HttpResponse("Hello world!")
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
