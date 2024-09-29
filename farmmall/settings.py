d330a5ea83280b52ca9ceb42cf0b44d4ce22e0cc
Griffin <omondigriffin4@gmail.com>
2024-09-29 10:47:09 +0200

    creates a requirements.txt file

--- farmmall/settings.py
+++ farmmall/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,11 +84,17 @@ WSGI_APPLICATION = 'farmmall.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.parse(
+        "postgresql://farmmall_db_user:skD2c6fiFEVvW47LvA3lyONaAF3d1uDQ@dpg-crsgsud6l47c73cvuts0-a.oregon-postgres.render.com/farmmall_db"
+    )
 }
 
 
