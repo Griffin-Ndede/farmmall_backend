737f34c5e44301594f300d3e6011c92809cfbc99
Griffin <omondigriffin4@gmail.com>
2025-02-28 09:11:55 +0300

    creates a new view for user

--- users/views.py
+++ users/views.py
@@ -1,11 +1,25 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework import status, viewsets
 from rest_framework_simplejwt.tokens import RefreshToken
 from .models import User, Activity
 from .serializers import UserRegistrationSerializer, UserLoginSerializer, ActivitySerializer
 from django.contrib.auth import authenticate
 import datetime
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def user_profile(request):
+    """
+    Retrieve logged-in user details.
+    """
+    user = request.user
+    serializer = UserRegistrationSerializer(user)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
 
 class RegisterView(APIView):
     """
